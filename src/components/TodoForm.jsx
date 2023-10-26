e849fffe99941f0a00bdc738ab5b180aab795690
ygorp <ygor-nogueira@hotmail.com>
2023-10-26 12:02:18 -0300

    criando a fucnionalidade do form para envio e consegui capturar as informações do input

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import { useState } from 'react';
 
 const todoForm = () => {
+    const [value, setValue] = useState("");
+    const [category, setCategory] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(value, category);
+    };
+
   return (
     <div>
         <h2>Criar tarefa:</h2>
-        <form>
-            <input type="text" placeholder='Digite o titulo' />
-            <select>
+        <form onSubmit={handleSubmit}>
+            <input type="text" placeholder='Digite o titulo' onChange={(e) => setValue(e.target.value)} />
+            <select onChange={(e) => setCategory(e.target.value)}>
                 <option value="">Selecione uma categoria</option>
                 <option value="Trabalho">Trabalho</option>
                 <option value="Pessoal">Pessoal</option>
