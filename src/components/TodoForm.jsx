1aac8a38218c6f6268a8dce4312ea2560517a471
ygorp <ygor-nogueira@hotmail.com>
2023-10-26 11:45:13 -0300

    criando um todo list com react

---
+++ src/components/TodoForm.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const todoForm = () => {
+  return (
+    <div>
+        <h2>Criar tarefa:</h2>
+        <form>
+            <input type="text" placeholder='Digite o titulo' />
+            <select>
+                <option value="">Selecione uma categoria</option>
+                <option value="Trabalho">Trabalho</option>
+                <option value="Pessoal">Pessoal</option>
+                <option value="Estudos">Estudos</option>
+            </select>
+            <button type="submit">Criar tarefa</button>
+        </form>
+    </div>
+  )
+}
+
+export default todoForm;
