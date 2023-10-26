e669495bb2eb551c5bca60d2889a560d785c2baf
ygorp <ygor-nogueira@hotmail.com>
2023-10-26 14:40:50 -0300

    criado a funcionalidade de completar e eexcluir tarefa

--- src/App.jsx
+++ src/App.jsx
@@ -40,15 +40,23 @@ function App() {
 };
 
 const removeTodo = (id) => {
-  
+  const newTodos = [...todos]
+  const filteredTodos = newTodos.filter(todo => todo.id !== id ? todo : null);
+  setTodos(filteredTodos);
 }
+
+const completeTodo = (id) => {
+  const newTodos = [...todos]
+  newTodos.map((todo) => todo.id === id ? todo.isCompleted = !todo.isCompleted : todo);
+  setTodos(newTodos);
+};
 
   return (
     <div className='app'>
       <h1>Lista de tarefas</h1>
       <div className="todo-list">
         {todos.map((todo) => (
-          <Todo key={todo.id} todo={todo} />
+          <Todo key={todo.id} todo={todo} removeTodo={removeTodo} completeTodo={completeTodo} />
         ))}
       </div>
       <TodoForm addTodo={addTodo} />
--- src/components/todo.jsx
+++ src/components/todo.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const todo = ({ todo }) => {
+const todo = ({ todo, removeTodo, completeTodo }) => {
   return (
-    <div className="todo">
+    <div className="todo" style={{textDecoration: todo.isCompleted ? "line-through" : ""}}>
         <div className="content">
             <p>{todo.text}</p>
             <p className="category">
@@ -10,8 +10,8 @@ const todo = ({ todo }) => {
             </p>
         </div>
         <div>
-            <button className='complete'>Completar</button>
-            <button className='remove'>X</button>
+            <button className='complete' onClick={() => completeTodo(todo.id)}>Completar</button>
+            <button className='remove' onClick={() => removeTodo(todo.id)}>X</button>
         </div>
     </div>
   )
